46556e8ee7d9755ae2ee03545ebeb1645775b320
annycavr <ancabrerany@gmail.com>
2023-08-02 22:11:41 -0600

    Proyecto Analisis Salarial

---
+++ promedio/analisis.js
@@ -0,0 +1,5 @@
+console.log(salarios);
+
+const persona salarios.find((persona) => {
+    return persona.name == ' '
+})
--- promedio/promedio.js
+++ promedio/promedio.js
@@ -1,25 +1,91 @@
-function calcularPromedio(lista){
+const mathProject = {};
+
+mathProject.calcularPromedio = function(lista){
    /*  let result = 0;
     for(let item of listaNumeros){
         result+= item;
     } */
 
-    const result = lista.reduce((valorAcumulado, valorActual) => valorAcumulado + valorActual);
+    const result = lista.reduce((valorAcumulado, valorActual) => 
+                valorAcumulado + valorActual, 0);
 
     return (result/lista.length);
 }
-//[1, 2, 3, 4]
 
-function esImpar (lista){
+mathProject.esImpar = function(lista){
     return lista.length % 2;
 }
 
-function calcularMediana (lista) {
+mathProject.calcularMediana = function(lista) {
+    lista.sort();
     const listaEsImpar = esImpar(lista);
 
     if(listaEsImpar){
-        
+        const positionMediana = Math.floor(lista.length/2);
+        return lista[positionMediana];
     } else {
-
+        const positionMediana = (lista.length/2);
+        return (calcularPromedio([lista[positionMediana-1], lista[positionMediana]]));
     }
+}
+
+mathProject.calcularModa = function(lista) {
+    const listCount = {};
+
+    for(let i = 0; i < lista.length; i ++) {
+        const element = lista[i];
+
+        if(listCount[element]){
+            listCount[element] += 1;
+        } else {
+            listCount[element] = 1;
+        }
+    }
+    
+    function sortArrayBidimensional (arrayUnsorted ,i) {
+        function ordenarLista(valorAcumulado, nuevoValor){
+            return  nuevoValor[i] - valorAcumulado[i] ;
+        }
+        const listSorted = arrayUnsorted.sort(ordenarLista);
+        return listSorted;
+    }
+
+    const listArray = Object.entries(listCount);
+    const listaOrdenada = sortArrayBidimensional(listArray, 1);
+    const listaMaxNumber = listaOrdenada[0];
+    const moda = listaMaxNumber[0];
+    return moda;
+    //console.log({listCount, listArray, listaOrdenada, listaMaxNumber});
+
+    //console.log('La moda es: ' + listaMaxNumber[0]);
+}
+
+
+mathProject.calcularMediaPonderada = function(){
+    let noteArray = [10, 8, 7];
+    let creditArray = [2, 5, 5];
+
+     function sumElements (noteArray, creditArray){
+        let result = 0 ;
+        for(var i = 0 ; i < noteArray.length; i++){
+            result+= (creditArray[i] * noteArray[i]);
+
+        }
+        return result;
+        
+     }
+
+     function sumDenominator (creditArray){
+        let result = 0 ;
+        for(var i = 0 ; i < creditArray.length; i++){
+            result+= creditArray[i];
+
+        }
+        return result;
+     }
+
+     const resultO = sumElements(creditArray, noteArray);
+     const resultD = sumDenominator(creditArray);
+     const result = resultO / resultD;
+     return console.log(`resultado: ${result}`  );
 }
